72863298a6bc2adef4b99da124f8307c4e69444b
tachikiH <137734056+tachikiH@users.noreply.github.com>
2023-12-05 14:25:37 +0900

    Update install_sqlite.sh

--- install_sqlite.sh
+++ install_sqlite.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 VERSION=$1
+YEAR=$2
 cd ~/workdir
-wget https://www.sqlite.org/2023/sqlite-src-${VERSION}.zip
+wget https://www.sqlite.org/${YEAR}/sqlite-src-${VERSION}.zip
 unzip sqlite-src-${VERSION}.zip
 cd sqlite-src-${VERSION}
 ./configure --enable-fts5
