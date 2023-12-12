cb4d0b5e0e40b0979e68fb6955f0bb0de93d67ae
tachikiH <137734056+tachikiH@users.noreply.github.com>
2023-12-12 18:08:43 +0900

    Update execute_test.sh

--- execute_test.sh
+++ execute_test.sh
@@ -4,7 +4,7 @@ cd ~/workdir/postgresql-${VERSION}/contrib/sqlite_fdw
 ./test.sh
 
 last_line=$(tail -n 1 make_check.out)
-third_linef_from_the_last=$(tail -n +3 filename | tail -n 1)
+third_linef_from_the_last=$(tail -n +3 make_check.out | tail -n 1)
 
 if [ "$last_line" != "# All 20 tests passed." -o "$third_linef_from_the_last" != " All 20 tests passed. "]; then
     echo "Error: The last line of make_check.out is not '# All 20 tests passed.'"
