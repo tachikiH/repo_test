0be387b4c3c40130b0e283d385cb392b8054c8fe
tachikiH <137734056+tachikiH@users.noreply.github.com>
2023-12-13 10:45:25 +0900

    Update execute_test.sh

--- execute_test.sh
+++ execute_test.sh
@@ -10,7 +10,7 @@ string_test_passed="All 20 tests passed"
 if [[ "$last_line" == *$string_test_passed* ]]; then
         echo "The last line of make_check.out contains '$string_test_passed'"
 
-elif [[ "$third_linef_from_the_last" == *$string_test_passed* ]]; then
+elif [[ "$third_line_from_the_last" == *$string_test_passed* ]]; then
         echo "The third line from the last of make_check.out contains '$string_test_passed'"
 else
         echo "Error : not All the tests passed"
