00c61452390e3a1956c8ae151bf62f87018667f6
tachikiH <137734056+tachikiH@users.noreply.github.com>
2023-12-13 11:20:12 +0900

    Update execute_test.sh

--- execute_test.sh
+++ execute_test.sh
@@ -4,7 +4,9 @@ cd ~/workdir/postgresql-${VERSION}/contrib/sqlite_fdw
 ./test.sh
 
 last_line=$(tail -n 1 make_check.out)
-third_line_from_the_last=$(tail -n +3 make_check.out | tail -n 1)
+third_line_from_the_last=$(tail -n 3 make_check.out | head -n 1)
+echo "$last_line"
+echo "$third_line_from_the_last"
 string_test_passed="All 20 tests passed"
 
 if [[ "$last_line" == *$string_test_passed* ]]; then
