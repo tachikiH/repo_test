0d4c1a11518fad7a20e69b80c8bdfa00628da833
tachikiH <137734056+tachikiH@users.noreply.github.com>
2023-12-13 09:53:51 +0900

    Update execute_test.sh

--- execute_test.sh
+++ execute_test.sh
@@ -7,7 +7,7 @@ last_line=$(tail -n 1 make_check.out)
 third_linef_from_the_last=$(tail -n +3 make_check.out | tail -n 1)
 string_test_passed="All 20 tests passed."
 
-if [[ $last_line != *$string_test_passed* ]] || [[ $third_linef_from_the_last != *$string_test_passed* ]]; then
+if [[ $last_line != *$string_test_passed* ]] && [[ $third_linef_from_the_last != *$string_test_passed* ]]; then
     echo "Error: The last line of make_check.out is not '# All 20 tests passed.'"
     exit 1
 fi
