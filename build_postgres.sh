f73115315e189e228d22300ce9ffc9bf5c914ce1
tachikiH <137734056+tachikiH@users.noreply.github.com>
2023-12-13 13:23:28 +0900

    Update build_postgres.sh

--- build_postgres.sh
+++ build_postgres.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#ファイルの概要
+#postgresをビルドするファイルです。
+
+#使い方
+#引数として、postgresqlのバージョン番号を指定してください 例えば、 16.0 など
+
+#前提条件
+#postgresqlのソースコードが配布URLからダウンロードできる必要があります。
+
 VERSION=$1
 mkdir ~/workdir
 cd ~/workdir
