49fca99c733893fad24ff56a3f6b1de6c4f05466
tachikiH <137734056+tachikiH@users.noreply.github.com>
2023-12-13 14:21:53 +0900

    Update install_locales.sh

--- install_locales.sh
+++ install_locales.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# ファイルの概要  
+#ubuntuの言語ファイル、ロケールファイルをインストールするファイルです。  
+
+# 使い方
+#テストをする際に必要になるロケールファイルを自動的に指定しています。  
+#特に引数などの指定をする必要なく、ただ実行するだけで処理が完了します。  
+
+# 前提条件  
+#GitHubActionsで用いる仮想マシンがUbuntuであること  
+
 sudo apt-get update
 sudo apt-get install locales language-pack-ja
 sudo locale-gen ja_JP.EUC-JP
