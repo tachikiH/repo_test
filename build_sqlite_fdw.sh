f52314e13dbc3711a4bdb14081d761b4420f0938
tachikiH <137734056+tachikiH@users.noreply.github.com>
2023-12-13 14:22:51 +0900

    Update build_sqlite_fdw.sh

--- build_sqlite_fdw.sh
+++ build_sqlite_fdw.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# ファイルの概要  
+#sqlite_fdwをビルドするファイルです。
+
+# 使い方
+#引数として、テストを行いたいpostgresqlのバージョン番号を指定してください ex. 16.0
+
+# 前提条件
+#githubのURLが利用可能であることが必要です。
+
 VERSION=$1
 cd ~/workdir/postgresql-${VERSION}/contrib
 git clone https://github.com/pgspider/sqlite_fdw.git
